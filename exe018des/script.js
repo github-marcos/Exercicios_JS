7b85658fd0a03ea0d6ee538a23df0a3dff0e5b27
Desafios js <marcosoliveiiraa@gmail.com>
2022-02-10 16:00:31 -0300

    des18c

--- exe018des/script.js
+++ exe018des/script.js
@@ -1,21 +1,29 @@
-var n = document.getElementById('d1')
-var n2 = document.getElementById('d2')
-var n3 = document.getElementById('d3')
 
+function carregar(){
 
- 
- function carregar(){
+var n = Number(document.getElementById('d1').value)
+var n2 = Number(document.getElementById('d2').value)
+var n3 = Number(document.getElementById('d3').value)
+var res = document.getElementById('res')
 
-  
-while(n <= n2){
-    
-     n3++
- }
+        if(n == 0 || n2 == 0 || n3 == 0){
+            window.alert('Preencha todos os campos.')
+        }else{
+            res.innerHTML = 'Contando: <br>'
+            //contagem crescente
+            if(n <= n3){
+            for(var c= n; c < n2; c += n3){
+            res.innerHTML +=`${c} \u{1f449} `
+            }
+            res.innerHTML += `${c} \u{1F3C1} `
+            //contagem decrescente
+            }else{
+        for( var c = n; c > n2; c -= n3) {   
+            res.innerHTML +=`${c} \u{1f449} `
+        }
+        res.innerHTML += `${c} \u{1F3C1} `
+           }  
+            
+        }
+}
 
- }
-
-
-
-/*for(n;n<=n2;n3++){
-
-}*/
