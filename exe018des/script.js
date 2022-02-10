7cf3ef6059a4daa6f462bd203b89dd4801e306dc
Desafios js <marcosoliveiiraa@gmail.com>
2022-02-09 22:49:35 -0300

    exe018

---
+++ exe018des/script.js
@@ -0,0 +1,21 @@
+var n = document.getElementById('d1')
+var n2 = document.getElementById('d2')
+var n3 = document.getElementById('d3')
+
+
+ 
+ function carregar(){
+
+  
+while(n <= n2){
+    
+     n3++
+ }
+
+ }
+
+
+
+/*for(n;n<=n2;n3++){
+
+}*/
