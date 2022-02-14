b26165111851bdec899773bda6c0eaef924cfb37
Desafios js <marcosoliveiiraa@gmail.com>
2022-02-14 16:35:53 -0300

    des19.2

---
+++ exe19desf/script.js
@@ -0,0 +1,17 @@
+function gerar(){
+var num = document.getElementById('txtn')
+var tab = document.getElementById('seltab')
+if ( num.value.length == 0){
+    window.alert('Escolha um número!') 
+}else{  
+    var n = Number(num.value)
+    var c = 1
+    tab.innerHTML= ''
+    while ( c <= 10){
+       var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n* c} `
+        tab.appendChild(item)
+        c++
+    }
+}
+}
